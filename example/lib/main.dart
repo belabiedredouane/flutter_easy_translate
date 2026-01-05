aad3988b673ff7f9d2106f2c1459c25bd370f83d
Belabied Redouane <48478700+belabiedredouane@users.noreply.github.com>
2026-01-05 22:20:20 +0100

    updating android project

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -23,6 +23,7 @@ class MyApp extends StatelessWidget {
         localizationsDelegates: [
           GlobalMaterialLocalizations.delegate,
           GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
           localizationDelegate
         ],
         supportedLocales: localizationDelegate.supportedLocales,
